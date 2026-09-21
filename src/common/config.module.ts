a6b28b468a622f2cd5f8eb6ae4bfae94d3caf334
Uday vara <varaudayd@gmail.com>
2026-09-21 13:12:24 +0530

    Added JWT config

--- src/common/config.module.ts
+++ src/common/config.module.ts
@@ -1,13 +1,20 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './services/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: ['.env.local', '.env'],
-  })],
+  }),
+  JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
