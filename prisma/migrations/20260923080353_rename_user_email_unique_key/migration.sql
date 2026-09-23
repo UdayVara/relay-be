2e4c8472073589db5c4a1efef30fb961d211e850
Uday vara <varaudayd@gmail.com>
2026-09-23 13:34:07 +0530

    added user key

---
+++ prisma/migrations/20260923080353_rename_user_email_unique_key/migration.sql
@@ -0,0 +1,2 @@
+-- RenameIndex
+ALTER INDEX "User_email_key" RENAME TO "user_email_unique";
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../common/services/prisma.service';
+import { SignupUserDto } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+
+    async SignupService(body:SignupUserDto){
+        try {
+            const checkUser = await this.prismaService.user.findUnique({
+                where: {
+                    email: body.email
+                }
+            });
+            if(checkUser){  
+                
+            }
+        } catch (error) {
+            
+        }
+    }
+}
